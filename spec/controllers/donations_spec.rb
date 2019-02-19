3d74f3d7ce08faf126c5ab6e9e6bd146bcc4cd74
Alex <alikhanialex@gmail.com>
2019-02-19 16:26:45 +0000

    test: passing test for donation index route

--- spec/controllers/donations_spec.rb
+++ spec/controllers/donations_spec.rb
@@ -1,31 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Api::V1::DonationsController do
-  # describe 'GET #index' do
-  #   it 'returns http success' do
-  #     get :index
-  #     expect(response).to have_http_status(:success)
-  #   end
-  describe 'Donations' do
-    it 'returns all objects' do
+  describe 'GET #index' do
+    it 'returns http success' do
       venue = Venue.create(name: 'Venue1', address: 'Address1')
-      
-      post :create, params: { venue_id: venue.id, donation: {amount: 10, passphrase: "Enjoy"} }
-      
-      expect(Donation.find_by(passphrase: "Enjoy")).to be
-
+      get :index, params: { venue_id: 1 }
+      expect(response).to have_http_status(:success)
     end
 
-    # it 'returns all objects' do
-    #   5.times do
-    #     Venue.create(name: 'Venue1', address: 'Address1')
-    #   end
-
-    #   get :index
-
-    #   venues = JSON.parse(response.body)
-    #   expect(venues.count).to eq(5)
-    # end
-    
+    describe 'POST /venues/:venue_id/donations' do
+      it 'creates a donation' do
+        venue = Venue.create(name: 'Venue2', address: 'Address2')
+        post :create, params: { venue_id: venue.id, donation: { amount: 10, passphrase: 'Enjoy' } }
+        expect(Donation.find_by(passphrase: 'Enjoy')).to be
+      end
+    end
   end
 end
