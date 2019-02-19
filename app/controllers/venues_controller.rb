b5895de64a50a9dea511a116c66f2795231ae799
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-19 19:30:17 +0000

    Geolocation: now saving lat and long from address

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -5,30 +5,36 @@ require 'json'
 class VenuesController < ApplicationController
 
     def index 
-        
+        @venues = Venue.all
     end 
 
     def create
-     
 
-        address = params[location]
-        p '********'
-        p address
-        clean_address(address)
-        find_address(address)
+        lat_long = find_address(params['name'])
+    
+        venue = Venue.new
+        venue.name = params['name']
+        venue.address = params['location']
+        venue.lattitude = lat_long['lat']
+        venue.longitude = lat_long['lng']
+        if venue.save
+          redirect_to '/venues'
+        end
     end 
 
     def find_address(address = '1600+Amphitheatre+Parkway,+Mountain+View')
        location = ask_google_api(address)
+       return location
        
     end 
+
 
     private 
 
     def ask_google_api(address)
 
-        url = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=address=1600+Amphitheatre+Parkway,
-        +Mountain+View&key=#{GOOGLE_API_KEY}")
+        address = clean_address(address)
+        url = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=#{address}&key=#{GOOGLE_API_KEY}")
 
         http = Net::HTTP.new(url.host, url.port)
         http.use_ssl = true
@@ -41,7 +47,8 @@ class VenuesController < ApplicationController
     
 
     def clean_address(address)
-        p address
         address = address.gsub(/\s+/, "+")
     end
+
+  
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,3 @@
 class Venue < ApplicationRecord
+    
 end
