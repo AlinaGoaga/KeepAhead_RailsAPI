182c7a96c29de07f93faad477a89781d8e5908d7
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-19 19:33:42 +0000

    CONFIG: allowed acess origin headers for fecth request

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,10 @@ module Socialproject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.action_dispatch.default_headers = {
+    'Access-Control-Allow-Origin' => '*',
+    'Access-Control-Request-Method' => %w{GET POST }.join(",")
+  }
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
