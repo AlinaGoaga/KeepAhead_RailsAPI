72a75c57d992fda37e377b31bb67fcdb631feb3f
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-19 14:24:56 +0000

    GEOLOCATION: started request

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -10,15 +10,21 @@ class VenuesController < ApplicationController
 
     def create
         address =  params[location]
+        find_address()
     end 
 
-    def find_address(address)
-        url = URI.parse("http://www.example.com/index.html")
-        req = Net::HTTP::Get.new(url.to_s)
-        res = Net::HTTP.start(url.host, url.port) {|http|
-            http.request(req)
-        }
-        puts res.body 
+    def find_address(address = '1600+Amphitheatre+Parkway,+Mountain+View')
+
+        url = URI.parse("https://maps.googleapis.com/maps/api/geocode/json?address=address=1600+Amphitheatre+Parkway,
+        +Mountain+View&key=AIzaSyBTwSpUW2WsDWTdODpCdxNMs1POdmpyI44")
+
+
+        http = Net::HTTP.new(url.host, url.port)
+        http.use_ssl = true
+        http.verify_mode = OpenSSL::SSL::VERIFY_NONE # You should use VERIFY_PEER in production
+        request = Net::HTTP::Get.new(url.request_uri)
+        res = http.request(request)
+        p res.body.results.formatted_address
     end 
 
 end
