8c808f52f82c2ba29361a5e6484bca71e54f2bcf
Alina Goaga <goaga.alina@gmail.com>
2019-02-19 17:02:41 +0000

    chore: heroku db migrate

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,25 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_219_144_101) do
+ActiveRecord::Schema.define(version: 2019_02_19_144101) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'donations', force: :cascade do |t|
-    t.integer 'amount'
-    t.string 'passphrase'
-    t.bigint 'venue_id'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['venue_id'], name: 'index_donations_on_venue_id'
+  create_table "donations", force: :cascade do |t|
+    t.integer "amount"
+    t.string "passphrase"
+    t.bigint "venue_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["venue_id"], name: "index_donations_on_venue_id"
   end
 
-  create_table 'venues', force: :cascade do |t|
-    t.string 'name'
-    t.string 'address'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "venues", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'donations', 'venues'
+  add_foreign_key "donations", "venues"
 end
