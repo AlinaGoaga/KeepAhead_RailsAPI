90ed9d8d1072ae3a7499e885ea6356e71657f9f4
Alina Goaga <goaga.alina@gmail.com>
2019-02-22 12:43:45 +0000

    Controller: added home and route index

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root to: 'home#index'
+
   namespace :api do
     namespace :v1 do
       resources :venues do
