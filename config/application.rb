3a60aa005e6fcea6ce6212246aefa174b0685caa
Alina Goaga <goaga.alina@gmail.com>
2019-02-25 11:57:55 +0000

    Updates to the models to work with React Stripe

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,6 @@ module Socialproject
         resource '*', :headers => :any, :methods => [:get, :post, :options]
       end
     end
-    config.middleware.use ActionDispatch::Flash
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
-# frozen_string_literal: true
-
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root to: 'home#index'
-  resources :charges, only: [:new, :create]
 
   namespace :api do
     namespace :v1 do
-      # resources :charges
       resources :venues do
         resources :donations
       end
