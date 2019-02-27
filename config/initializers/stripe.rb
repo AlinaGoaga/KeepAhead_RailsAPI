72bc622d4a95998dbb5acee3664b840bbfd83974
Alina Goaga <goaga.alina@gmail.com>
2019-02-27 18:42:39 +0000

    Updated config for API - 2

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_TYooMQauvdEDq54NiTphI7jx',
-  :secret_key      => 'sk_test_4eC39HqLyjWDarjtT1zdp7dc'
+  :publishable_key => ENV['PUBLISHABLE_KEY'],
+  :secret_key      => ENV['SECRET_KEY']
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
